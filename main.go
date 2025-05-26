19d67636c34d551b9750cedb55ea1dfcf51f97a8
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-26 14:56:55 -0500

    go

--- main.go
+++ main.go
@@ -7,25 +7,13 @@ import (
 )
 
 func main() {
-	// Manejador para la ruta principal
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "¡Hola! Este es un servidor web básico en Go.")
+		fmt.Fprintf(w, "¡Hola!")
 	})
 
-	// Manejador para la ruta /saludo
-	http.HandleFunc("/saludo", func(w http.ResponseWriter, r *http.Request) {
-		nombre := r.URL.Query().Get("nombre")
-		if nombre == "" {
-			nombre = "Visitante"
-		}
-		fmt.Fprintf(w, "¡Hola, %s! Bienvenido al servidor Go.", nombre)
-	})
+	port := ":3005"
+	log.Printf("Servidor en http://localhost%s\n", port)
 
-	// Configurar el puerto
-	port := ":8080"
-	log.Printf("Servidor escuchando en http://localhost%s\n", port)
-	
-	// Iniciar el servidor
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Error al iniciar el servidor: %v", err)
 	}
